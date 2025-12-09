1b644ef57332915fc89bae99df192f699ae04f34
unryaton <136084418+unryaton@users.noreply.github.com>
2025-12-10 00:18:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'gyy123k.unryaton.qzz.io';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiODMxZGZiODVjOTRlYjE4ZDIwOTY5MjBhNTI5YTMxNDIiLCJ0IjoiOTllNzJlN2ItNDU0Yy00M2IwLWIyOGEtZjI2YTU5MGE4MGE2IiwicyI6Ik5EQmlOVGRqTlRFdFpUUTNZUzAwTnpFd0xUa3haREF0TWpVM016SXdaak16WVRBNCJ9'
-';            // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiODMxZGZiODVjOTRlYjE4ZDIwOTY5MjBhNTI5YTMxNDIiLCJ0IjoiOTllNzJlN2ItNDU0Yy00M2IwLWIyOGEtZjI2YTU5MGE4MGE2IiwicyI6Ik5EQmlOVGRqTlRFdFpUUTNZUzAwTnpFd0xUa3haREF0TWpVM016SXdaak16WVRBNCJ9';   
+         // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
